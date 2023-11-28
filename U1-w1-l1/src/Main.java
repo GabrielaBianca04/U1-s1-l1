d46135d2d033b7e616f586654264f3ce7ca498c9
GabrielaBianca04 <143381847+GabrielaBianca04@users.noreply.github.com>
2023-11-28 15:06:14 +0100

    Add files via upload

---
+++ U1-w1-l1/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 4, b = 6;
+        int risultato = a*b;
+        System.out.println("Stampo il prodotto: " + risultato);
+
+        int x = 2;
+        String y = "Hello";
+        System.out.println(y + x));
+
+        String[] arrayDiStringhe = {"Hello", "my", "name", "is", "Bianca"};
+
+        }
+    }
