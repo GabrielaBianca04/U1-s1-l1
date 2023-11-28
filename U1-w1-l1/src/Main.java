b68d5d4275c1baae8a2796921adc68e6b3d78422
GabrielaBianca04 <143381847+GabrielaBianca04@users.noreply.github.com>
2023-11-28 15:23:17 +0100

    Add files via upload

--- U1-w1-l1/src/Main.java
+++ U1-w1-l1/src/Main.java
@@ -5,11 +5,29 @@ public class Main {
         int risultato = a*b;
         System.out.println("Stampo il prodotto: " + risultato);
 
-        int x = 2;
+
         String y = "Hello";
-        System.out.println(y + x));
+        System.out.println(concat(y , 2));
 
         String[] arrayDiStringhe = {"Hello", "my", "name", "is", "Bianca"};
 
         }
+
+    public static void main2(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] arrayDiStringhe = new String[3];
+        System.out.println("Componi una stringa");
+        arrayDiStringhe[0] = scan.nextLine();
+        System.out.println("Componi una seconda stringa");
+        arrayDiStringhe[1] = scan.nextLine();
+        System.out.println("Componi una terza stringa");
+        arrayDiStringhe[2] = scan.nextLine();
+
+        System.out.println("Here the strings concatenated");
+        System.out.println( arrayDiStringhe[0] + arrayDiStringhe[1] + arrayDiStringhe[2]);
+
+
+
+
+    }
     }
